3985489ce76636c43d2c7f486d66c216c2dce984
Marc416 <juni416@naver.com>
2025-05-10 22:54:22 +0900

    feat : application.yaml 환경별로 나누기

--- backend/election/vote/src/test/kotlin/com/futiland/vote/application/account/controller/QueryControllerTest.kt
+++ backend/election/vote/src/test/kotlin/com/futiland/vote/application/account/controller/QueryControllerTest.kt
@@ -9,11 +9,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 import kotlin.test.Test
 
 @SpringBootTest
+@ActiveProfiles("h2-test")
 @Transactional
 class QueryControllerTest {
 
