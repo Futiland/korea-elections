15975214323b39cd65d46bfd18e9ce46d3cf46cc
Marc416 <juni416@naver.com>
2025-05-10 16:56:49 +0900

    test : Signin, Signup 테스트

---
+++ backend/election/vote/src/test/kotlin/com/futiland/vote/application/account/repository/FakeAccountRepository.kt
@@ -0,0 +1,39 @@
+package com.futiland.vote.application.account.repository
+
+import com.futiland.vote.domain.account.entity.Account
+import com.futiland.vote.domain.account.repository.AccountRepository
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaField
+
+class FakeAccountRepository: AccountRepository {
+    val map = mutableMapOf<Long, Account>()
+    override fun save(account: Account): Account {
+        val id = generateId()
+
+        // Kotlin 리플렉션으로 id 필드 설정 (JDBC 구현체들이 동일하게 리플렉션으로 ID를 바꾸므로 테스트코드도 동일하게 변경)
+        val kClass = account::class
+        val idProperty = kClass.memberProperties.firstOrNull { it.name == "id" }
+
+        idProperty?.let {
+            it.javaField?.isAccessible = true
+            it.javaField?.set(account, id)
+        }
+
+        map[id] = account
+        return account
+    }
+
+    override fun getById(id: Long): Account {
+        return map[id] ?: throw Exception("Account not found")
+    }
+
+    override fun getByPhoneNumberAndPassword(phoneNumber: String, password: String): Account {
+        val account = map.values.find { it.phoneNumber == phoneNumber && it.password == password }
+            ?: throw Exception("Account not found")
+        return account
+    }
+
+    private fun generateId(): Long {
+        return map.keys.lastOrNull()?.plus(1) ?: 1L
+    }
+}
---
+++ backend/election/vote/src/test/kotlin/com/futiland/vote/domain/account/service/AccountCommandServiceTest.kt
@@ -0,0 +1,102 @@
+package com.futiland.vote.domain.account.service
+
+import com.futiland.vote.application.account.repository.FakeAccountRepository
+import com.futiland.vote.application.common.JwtTokenProviderImpl
+import com.futiland.vote.application.dto.response.IdentityVerifiedInfoResponse
+import com.futiland.vote.domain.account.entity.Account
+import com.futiland.vote.domain.account.entity.Gender
+import com.futiland.vote.domain.account.repository.AccountRepository
+import com.futiland.vote.domain.common.JwtTokenProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import java.time.LocalDate
+import kotlin.test.Test
+
+class AccountCommandServiceTest {
+
+    lateinit var accountCommandUseCase: AccountCommandUseCase
+    lateinit var accountRepository: AccountRepository
+    lateinit var jwtTokenProvider: JwtTokenProvider
+    val SECRET_KEY = "SECRET_KEY"
+
+    @BeforeEach
+    fun setUp() {
+        this.accountRepository = FakeAccountRepository()
+        this.jwtTokenProvider = JwtTokenProviderImpl(
+            secretKey = SECRET_KEY,
+        )
+        this.accountCommandUseCase = AccountCommandService(
+            accountRepository = accountRepository,
+            jwtTokenProvider = jwtTokenProvider,
+            accessTokenTtl = 3600
+        )
+    }
+
+    @Nested
+    inner class `회원가입` {
+        @Test
+        fun `회원가입 성공`() {
+            // Arrange
+            val account = Account.create(
+                phoneNumber = "01012345678",
+                password = "password",
+                gender = Gender.MALE,
+                birthDate = LocalDate.now(),
+                ci = "ci",
+            )
+            // Action
+            val newAccount = accountCommandUseCase.singUp(
+                phoneNumber = account.phoneNumber,
+                password = account.password,
+                identityVerifiedInfoResponse = IdentityVerifiedInfoResponse(
+                    gender = account.gender,
+                    birthDate = account.birthDate,
+                    ci = account.ci,
+                )
+            )
+
+            // Assert
+            val savedAccount = accountRepository.getByPhoneNumberAndPassword(
+                phoneNumber = account.phoneNumber,
+                password = account.password
+            )
+            assertThat(newAccount.id).isEqualTo(savedAccount.id)
+        }
+    }
+
+    @Nested
+    inner class `로그인` {
+        @Test
+        fun `로그인 성공`() {
+            // Arrange
+            val account = Account.create(
+                phoneNumber = "01012345678",
+                password = "password",
+                gender = Gender.MALE,
+                birthDate = LocalDate.now(),
+                ci = "ci",
+            )
+            val newAccount = accountCommandUseCase.singUp(
+                phoneNumber = account.phoneNumber,
+                password = account.password,
+                identityVerifiedInfoResponse = IdentityVerifiedInfoResponse(
+                    gender = account.gender,
+                    birthDate = account.birthDate,
+                    ci = account.ci,
+                )
+            )
+
+            // Action
+            val signInResponse = accountCommandUseCase.signIn(
+                phoneNumber = account.phoneNumber,
+                password = account.password
+            )
+
+            // Assert
+            assertThat(signInResponse.token).isNotNull()
+        }
+
+    }
+}
