752465c8e84ec6f6490857e00213cf98a5094a75
Marc416 <juni416@naver.com>
2025-05-16 18:31:51 +0900

    feat : 임시로 클라이언트 작업 home 만들기 "/"

--- backend/election/vote/build.gradle.kts
+++ backend/election/vote/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("com.auth0:java-jwt:4.1.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
