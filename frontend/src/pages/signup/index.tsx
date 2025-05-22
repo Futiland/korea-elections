eff1b5756ab2e6074a7d29d717a2d7d7cd783b5e
changuri <chaehyun282@naver.com>
2025-05-22 11:14:41 +0900

    add introducelayout component

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -7,6 +7,8 @@ import { useRouter } from 'next/router';
 import { useMutation } from '@tanstack/react-query';
 import { signup } from '@/lib/api/account';
 import type { SignupData } from '@/lib/types/account';
+import IntroduceLayout from '@/components/IntroduceLayout';
+
 export default function SignupPage() {
 	const router = useRouter();
 
@@ -69,38 +71,8 @@ export default function SignupPage() {
 				</div>
 
 				{/* 정보 안내 영역 */}
-				<div className="w-full max-w-lg mx-auto mt-6 space-y-5 bg-slate-100">
-					<div className="text-center ">
-						<h2 className="text-lg font-semibold mb-1 py-7.5">
-							KEP는 어떤 서비스 인가요 ?
-						</h2>
-						<p className="text-md text-gray-500">
-							“우리는 선거의 공정성과 시민의 의견을 최우선으로 생각합니다.
-							신뢰할 수 있는 연구를 위해 철저하게 설계된 프로젝트입니다.”
-						</p>
-					</div>
-
-					<div className="bg-white p-4 rounded-md shadow-sm">
-						<h3 className="font-semibold mb-1">
-							개인정보는 제3자에게 절대 제공되지 않습니다.
-						</h3>
-						<p className="text-sm text-muted-foreground">
-							본 조사는 완전한 익명으로 진행되며, 수집된 정보는 오직 연구 목적에
-							한하여 사용됩니다. 개인정보는 저장되지 않으며, 제3자에게 절대
-							제공되지 않습니다.
-						</p>
-					</div>
-
-					<div className="bg-white p-4 rounded-md shadow-sm">
-						<h3 className="font-semibold mb-1">
-							어떤 정보도 외부에 제공되지 않습니다.
-						</h3>
-						<p className="text-sm text-muted-foreground">
-							Korea Elections Project는 익명으로 참여할 수 있으며, 어떤 정보도
-							외부에 제공되지 않습니다. 공정하고 안전한 선거 문화 확산을 위해
-							만들어진 시스템입니다.
-						</p>
-					</div>
+				<div className="max-w-lg mx-auto py-8 px-7 bg-slate-100">
+					<IntroduceLayout />
 				</div>
 			</div>
 		</>
