5d6577519d01bdf8fbe663309f72e6547941f549
채현 <52442881+chaengguri@users.noreply.github.com>
2025-05-22 16:13:33 +0900

    Feat/add introduce component (#14)
    * add introducelayout component
    
    * add introduce dialog
    
    * login page ui 수정
    
    * my page ui 수정

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,24 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Button } from '@/components/ui/button';
-import {
-	DropdownMenu,
-	DropdownMenuTrigger,
-	DropdownMenuContent,
-	DropdownMenuItem,
-} from '@/components/ui/dropdown-menu';
 import { Menu } from 'lucide-react';
 import Image from 'next/image';
+import { useState } from 'react';
+
+import IntroduceLayout from '@/components/IntroduceLayout';
+import { Dialog, DialogContent, DialogFooter } from '@/components/CustomDialog';
 
 export default function Header() {
 	const router = useRouter();
 
+	const [isOpen, setIsOpen] = useState(false);
+
+	const visbleIntroduceDialog = () => {
+		setIsOpen(true);
+	};
+
 	return (
-		<header className="w-full border-b bg-slate-100 dark:bg-gray-950">
+		<header className="w-full border-b bg-slate-50 dark:bg-gray-950 shadow-sm">
 			<div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3">
 				{/* 로고 */}
 				<Link href="/">
@@ -28,11 +32,30 @@ export default function Header() {
 
 				{/* 우측 */}
 				<div className="flex items-center gap-2">
-					<Button variant="outline" size="sm" className="py-1 px-4">
+					<Button
+						variant="outline"
+						size="sm"
+						className="py-1 px-4"
+						onClick={visbleIntroduceDialog}
+					>
 						KEP 프로젝트 소개
 					</Button>
 				</div>
 			</div>
+
+			<Dialog open={isOpen} onOpenChange={setIsOpen}>
+				<DialogContent className="bg-slate-100 px-5 py-6">
+					<IntroduceLayout />
+					<DialogFooter>
+						<Button
+							className="w-full bg-blue-900 text-white hover:bg-blue-800"
+							onClick={() => setIsOpen(false)}
+						>
+							확인
+						</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
 		</header>
 	);
 }
