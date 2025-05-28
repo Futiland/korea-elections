5494d22c8e5ddc558abd3f73abf8824e6d7b7ca0
changuri <chaehyun282@naver.com>
2025-05-28 13:58:04 +0900

    add fgooter

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,31 +1,28 @@
-import Link from 'next/link';
-import { Phone, Building2 } from 'lucide-react';
-
+import Image from 'next/image';
 export default function Footer() {
 	return (
-		<footer className="w-full border-t bg-white dark:bg-gray-950">
-			<div className="mx-auto max-w-7xl px-4 py-6 mb-14 text-sm text-muted-foreground flex flex-col md:flex-row justify-between items-center gap-2">
-				<div className="flex gap-4">
-					<div className="flex gap-2 justify-center items-center">
-						<Phone className="w-4 h-4" />
-						<p>0507-1322-0033</p>
-					</div>
-					<div className="flex gap-2 justify-center items-center">
-						<Building2 className="w-4 h-4" />
-						<p>105-60-00842 (사업자등록번호)</p>
-					</div>
+		<footer className="w-full bg-slate-100 text-slate-400 text-sm px-6 pb-20">
+			<div className="w-full flex items-center justify-center py-6">
+				<div className="relative w-[76px] h-[24px] ">
+					<Image
+						src="/img/logo-g.svg"
+						alt="KEP 로고"
+						fill
+						className="object-contain"
+					/>
 				</div>
-				<div className="flex gap-4">
-					<Link href="/privacy" className="hover:underline">
-						개인정보처리방침
-					</Link>
-					<Link href="/terms" className="hover:underline">
-						이용약관
-					</Link>
+			</div>
+			<div>
+				<div className="space-y-1 leading-relaxed flex flex-col items-start font-semibold">
+					<p>상호명 : 푸티랜드(Futiland)</p>
+					<p>대표자 : 이준희</p>
+					<p>주소 : 서울특별시 강동구 천중로 264, 102동 21층 2106호</p>
+					<p>사업자등록번호 : 105-60-00842</p>
+					<p>전화번호 : 0507-1322-0033</p>
 				</div>
-				<div>
-					&copy; {new Date().getFullYear()} Korea Elections. All rights reserved
-				</div>
+			</div>
+			<div className="py-4 text-center">
+				&copy; {new Date().getFullYear()} Korea Elections. All rights reserved
 			</div>
 		</footer>
 	);
