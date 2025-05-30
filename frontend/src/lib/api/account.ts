80829e21961eab5d5ef4b0ebbf36db6f771a4fc5
changuri <chaehyun282@naver.com>
2025-05-30 11:35:24 +0900

    add verification stopper

--- frontend/src/lib/api/account.ts
+++ frontend/src/lib/api/account.ts
@@ -5,6 +5,7 @@ import {
 	UserInfo,
 	SignupRequestData,
 	SignupResponse,
+	SignupStopperResponse,
 } from '../types/account';
 
 export const login = (data: LoginData) =>
@@ -15,3 +16,6 @@ export const getUserInfo = () =>
 
 export const signup = (data: SignupRequestData) =>
 	apiPost<SignupResponse>('/rest/account/v1/signup', data);
+
+export const signupStopper = () =>
+	apiGet<SignupStopperResponse>('/rest/account/v1/stopper');
