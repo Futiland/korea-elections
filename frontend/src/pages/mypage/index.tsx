72f173704ac780efb63e484a86c650d2bcc58c54
채현 <52442881+chaengguri@users.noreply.github.com>
2026-02-04 13:52:57 +0900

    update mypage (#189)

--- frontend/src/pages/mypage/index.tsx
+++ frontend/src/pages/mypage/index.tsx
@@ -1,5 +1,5 @@
 import { useMyPagePresenter } from '@/components/My/useMyPagePresenter';
-import MyPageView from '../../components/My/MyPageView';
+import MyPageView from '@/components/My/MyPageView';
 
 export default function MyPage() {
 	const presenterProps = useMyPagePresenter();
